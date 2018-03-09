9b31d76eba7cae285266ce58e4cad8ad7cfeb4a3
Ryuhos <ryu3223@naver.com>
2018-03-09 14:10:13 +0900

    Main file

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
 # main
+import sys
+sys.path.append('D:/paperstudy/start/optimizer.py')
+sys.path.append('D:/paperstudy/start/layers.py')
 
 import numpy as np
 import pandas as pd
-# import os
-# os.getcwd()
+from optimizer import Optimizer
+from layers import make_w
 
 # iris data 불러오기
 data = pd.read_csv("iris.csv")
 x = data.values[:,:4]
 y = data.values[:,4]
+y_onehot = np.array(pd.get_dummies(y))
+W = make_w(x,y_onehot)
 
-# w1 shape(#column_of_x,hidden1_node) 생성
-# w2 shape(hidden1_node,hidden2_node) 생성
-# w3 shape(hidden2_node,y_label) 생성
-
-# h1 = np.dot(x,w1)
-# H1 = ActivationFunction(h1)
-# H1 = H1.sigmoid()
-# h2 = np.dot(H1,w2)
-# H2 = ActivationFunction(h2)
-# H2 = H2.sigmoid()
-# h3 = np.dot(H2,w3)
-# y_hat = ActivationFunction(h3)
-# y_hat = y_hat.softmax()
-# loss = LossFunction(y,y_hat)
-# loss = loss.cross_entropy()
-
-# optimizer = optimizer(layer)
-# optimizer = optimizer.Gradient()
-# 이거 layer 를 받아들이게 만들면 일반화가 되겠네 그러면 layer 클래스를 만들어 보자
-
-# 결과값은 w = [] , loss
+model1 = Optimizer(x,y,W)
+model1.Gradient_relu(epochs=100,lr=0.05)
